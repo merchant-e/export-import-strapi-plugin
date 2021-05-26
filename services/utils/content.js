544152ad66e11f2fdb019c29cedd29b154c93505
Ivan Jimenez <ivanji@gmail.com>
2021-05-25 19:53:55 -0500

    Fixes and improvements to localities imports

--- services/utils/content.js
+++ services/utils/content.js
@@ -1,40 +1,43 @@
 // Default: Normal imports
 const importItemByContentType = async (id, item) => {
+  // If importing departments
+  if (id === "application::departments.departments") {
+    const deptRegionRelationship = await processDepartments(id, item);
+    return deptRegionRelationship;
+  } else if (id === "application::cities.cities") {
+    // when importing cities
+    const cities = processCities(id, item);
+    return cities;
+  }
+  // Normal collections
   return strapi.query(id).create(item);
 };
 
 // When Importing departments. Department: strapi-departments-regions.json (name, region)
-// const importItemByContentType = async (id, item) => {
-//   // when importing cities let's process them differently
+const processDepartments = async (id, item) => {
+  const region = await getRegion(item);
+  console.log(`%cAssigning ${region.name} to ${item.name}`, "color: green");
 
-//   // console.log(item);
-//   // const department = await getDepartment(item);
-//   const region = await getRegion(item);
-//   console.log(
-//     `%cAssigning Department: ${departmentId.name} and ${region.name} to ${item}`,
-//     "color: green"
-//   );
-
-//   return strapi.query(id).create({
-//     ...item,
-//     region: region.id,
-//   });
-// };
+  return strapi.query(id).create({
+    ...item,
+    region: region.id,
+  });
+};
 
 // When importing Cities
-// const importItemByContentType = async (id, item) => {
-//   // Let's get department and region for each item being imported
-//   const departmentId = await getDepartment(item);
-//   const regionId = await getRegion(item);
-//   console.log(
-//     `%cAssigning Department: ${departmentId.name} and ${regionId.name} to ${item.shortLabel}`,
-//     "color: green"
-//   );
+const processCities = async (id, item) => {
+  // Let's get department and region for each item being imported
+  const department = await getDepartment(item);
+  const region = await getRegion(item);
+  console.log(
+    `%cAssigning Department: ${department.name} and ${region.name} to ${item.shortLabel}`,
+    "color: green"
+  );
 
-//   return strapi
-//     .query(id)
-//     .create({ ...item, department: departmentId, region: regionId });
-// };
+  return strapi
+    .query(id)
+    .create({ ...item, department: department.id, region: region.id });
+};
 
 // Gets department for current item being imported and returns its db id
 const getDepartment = async (item) => {
